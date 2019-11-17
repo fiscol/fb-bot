1e26584beab348395d68bc9520e14515c4b4e105
吳浩平 <wuhaoping@wuhaopingdeMacBook-Pro.local>
2019-11-17 17:21:34 +0800

    change datetime package

--- tool/TimeNow.js
+++ tool/TimeNow.js
@@ -1,80 +1,19 @@
 // // Show Time Now
-const time = require('time'); 
+const time = require('dateformat'); 
 
 module.exports = {
   // 2017/03/30 22:35:12
   TimeNow: function(){
-    var now = new time.Date();
-    now.setTimezone("Asia/Taipei");
-    var TimeSplit = now.toString().split(" ");
-    var Month = monthTrans(TimeSplit[1]);
-    var TimeOutput = TimeSplit[3] + '/' + Month + '/' + TimeSplit[2] + ' ' + TimeSplit[4];
-    return TimeOutput.toString();
+    return time(new Date(), "yyyy/mm/dd HH:MM:ss");
   },
 
   // 2017/03/30
   DateNow : function(){
-    var now = new time.Date();
-    now.setTimezone("Asia/Taipei");
-    var TimeSplit = now.toString().split(" ");
-    var Month = monthTrans(TimeSplit[1]);
-    var DateOutput = TimeSplit[3] + '/' + Month + '/' + TimeSplit[2];
-    return DateOutput.toString();
+    return time(new Date(), "yyyy/mm/dd");
   },
 
   // 2017-03-30
   DateNowDBChild : function(){
-    var now = new time.Date();
-    now.setTimezone("Asia/Taipei");
-    var TimeSplit = now.toString().split(" ");
-    var Month = monthTrans(TimeSplit[1]);
-    var DateDBChildOutput = TimeSplit[3] + '-' + Month + '-' + TimeSplit[2];
-    return DateDBChildOutput.toString();
+    return time(new Date(), "yyyy-mm-dd");
   },
-  
-}
-
-
-// Month transform
-function monthTrans(Month){
-  var MonthNum;
-  switch(Month){
-    case 'Jan':
-      MonthNum = '01';
-      break;
-    case 'Feb':
-      MonthNum = '02';
-      break;
-    case 'Mar':
-      MonthNum = '03';
-      break;
-    case 'Apr':
-      MonthNum = '04';
-      break;
-    case 'May':
-      MonthNum = '05';
-      break;
-    case 'Jun':
-      MonthNum = '06';
-      break;
-    case 'Jul':
-      MonthNum = '07';
-      break;
-    case 'Aug':
-      MonthNum = '08';
-      break;
-    case 'Sep':
-      MonthNum = '09';
-      break;
-    case 'Oct':
-      MonthNum = '10';
-      break;
-    case 'Nov':
-      MonthNum = '11';
-      break;
-    case 'Dec':
-      MonthNum = '12';
-      break;
-  }
-  return MonthNum;
 }
